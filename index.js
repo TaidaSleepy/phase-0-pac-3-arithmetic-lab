dbf0e12d6aaf02d301d5d57d986c55412d981f16
Taida <samuelgbusinessacc@gmail.com>
2025-08-31 10:33:10 +0300

    adds index.js

---
+++ index.js
@@ -0,0 +1,46 @@
+1 + 80; //=> 81
+60 - 40; //=> 20
+2 * 3.4; //=> 6.8
+5.0 / 2.5; //=> 2
+function add() {
+  // we'll fill this in shortly
+}
+function subtract() {
+  // we'll fill this in shortly
+}
+function multiply() {
+  // we'll fill this in shortly
+}
+function divide() {
+  // we'll fill this in shortly
+}
+// Write your functions here:
+function add(num1, num2) {
+  return num1 + num2;
+}
+function subtract(num1, num2) {
+  return num1 - num2;
+}
+function multiply(num1, num2) {
+  return num1 * num2;
+}
+function divide(num1, num2) {
+  return num1 / num2;
+}
+function add(a, b) {
+  return a + b;
+}
+function increment(n) {
+  return n + 1;
+}
+
+// decrement.js  
+function decrement(n) {
+  return n - 1;
+}
+function makeInt(n) {
+  return parseInt(n, 10); // Base 10 parsing
+}
+function preserveDecimal(n) {
+  return parseFloat(n);
+}
